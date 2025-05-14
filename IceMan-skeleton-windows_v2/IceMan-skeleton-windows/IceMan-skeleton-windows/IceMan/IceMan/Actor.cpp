5a7dc04776feb63ee1b81fd6c50b48ba2fb13fa8
PaiggiePie <paiggiepie@gmail.com>
2025-05-14 13:30:06 -0700

    Testing actor.cpp

--- IceMan-skeleton-windows_v2/IceMan-skeleton-windows/IceMan-skeleton-windows/IceMan/IceMan/Actor.cpp
+++ IceMan-skeleton-windows_v2/IceMan-skeleton-windows/IceMan-skeleton-windows/IceMan/IceMan/Actor.cpp
@@ -1,4 +1,8 @@
 #include "Actor.h"
 #include "StudentWorld.h"
 
+int main () {
+    cout << "Hello, World!" << endl;
+    cout << "This is a test." << endl;
+}
 // Students:  Add code to this file (if you wish), Actor.h, StudentWorld.h, and StudentWorld.cpp
