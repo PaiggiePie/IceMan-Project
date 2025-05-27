dbe2ed6c4c60ead8efd9694fd762302829e54003
PaiggiePie <paiggiepie@gmail.com>
2025-05-27 12:39:05 -0700

    1st change

--- IceMan-skeleton-windows_v2/IceMan-skeleton-windows/IceMan-skeleton-windows/IceMan/IceMan/Actor.h
+++ IceMan-skeleton-windows_v2/IceMan-skeleton-windows/IceMan-skeleton-windows/IceMan/IceMan/Actor.h
@@ -3,6 +3,101 @@
 
 #include "GraphObject.h"
 
-// Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
+class Actor : public GraphObject {
+	Actor(int imageID, int startX, int startY,
+		DIRECTION startDirection, float size,
+		unsigned int depth);
+	void setVisible(bool shouldIDisplay); 
+	void getX() const;
+	void moveTo(int x, int y);
+	void doSomething();
 
+protected: 
+	size_t current_level_number; // starts at 1
+	// int?
+};
+
+class Iceman : public Actor {
+	// bonked by boulders
+
+	Iceman(int imageID, int startX, int startY,
+		DIRECTION startDirection, float size,
+		unsigned int depth) : Actor(IID_PLAYER, 30, 60, "right", 1.0, 0);
+
+	int getX() const;
+	int getY() const; // uses GraphObject x, y member variables
+	void moveTo(int x, int y);
+	DIRECTION getDirection() const;
+	void setDirection(DIRECTION d);
+
+private:
+	int water_count = 5;
+	int hit_points = 10;
+	int sonar_charge = 1;
+	int gold_nuggets = 0;
+
+};
+
+class Protester : public Actor {
+	// (60, 60)
+	int probabilityOfHardcore = std::min(90, current_level_number * 10 + 30);
+	int T = std::max(25, 200 – current_level_number); // # of tick to wait for new protester
+	int P = std::min(15, 2 + current_level_number * 1.5); // # of Protesters that should be on field
+};
+
+class RegularProtesters : public Protester { 
+	// annoyed by squirts of water
+	// bonked by boulders
+
+	void moveTo(int x, int y);
+	// moveTo(getX()+1, y); 
+	// move one square to the right
+};
+
+class HardcoreProtesters : public Protester {
+	// annoyed by squirts of water
+	// bonked by boulders
+	
+
+};
+
+class StaticObject : public Actor {
+	
+};
+
+class SquirtsOfWater : public DynamicObject {
+
+};
+
+class BarrelsOfOil : public StaticObject {
+
+};
+
+class Boulders : public DynamicObject {
+
+};
+
+class GoldNuggets : public StaticObject {
+
+};
+
+class SonarKits : public StaticObject {
+	// (0, 60)
+	// chance of spawn:  int G = current_level_number * 30 + 290
+
+};
+
+class DynamicObject : public Actor {
+
+};
+
+class Water : public DynamicObject { // water refills (water pools)
+	// location: random ice-less spot in 4x4 grid
+	// 4/5 chance
+	// chance of spawn:  int G = current_level_number * 30 + 290
+};
+
+class Ice : public StaticObject {
+
+};
 #endif // ACTOR_H_
--- IceMan-skeleton-windows_v2/IceMan-skeleton-windows/IceMan-skeleton-windows/IceMan/IceMan/StudentWorld.h
+++ IceMan-skeleton-windows_v2/IceMan-skeleton-windows/IceMan-skeleton-windows/IceMan/IceMan/StudentWorld.h
@@ -4,6 +4,7 @@
 #include "GameWorld.h"
 #include "GameConstants.h"
 #include <string>
+#include <iomanip>
 
 // Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
 
@@ -28,11 +29,50 @@ public:
 		return GWSTATUS_PLAYER_DIED;
 	}
 
-	virtual void cleanUp()
-	{
+	virtual void cleanUp();
+
+	virtual const int getCurrentGameLevel() { return gameLevel; };
+	virtual const int getNumLivesLeft() { return lives; };
+	virtual const int getCurrentHealth() { return health; };
+	virtual const int getSquirtsLeftInSquirtGun() { return squirtGun; };
+	virtual const int getPlayerGoldCount() { return gold; };
+	virtual const int getNumberOfBarrelsRemainingToBePickedUp() { return barrels; };
+	virtual const int getPlayerSonarChargeCount() { return sonarCharge; };
+	virtual const int getCurrentScore() { return score; };
+
+	virtual void setDisplayText() {
+		int level = getCurrentGameLevel();
+		int lives = getNumLivesLeft();
+		int health = getCurrentHealth();
+		int squirts = getSquirtsLeftInSquirtGun();
+		int gold = getPlayerGoldCount();
+		int barrelsLeft = getNumberOfBarrelsRemainingToBePickedUp();
+		int sonar = getPlayerSonarChargeCount();
+		int score = getCurrentScore();
+
+		// Next, create a string from your statistics, of the form: 
+		// Lvl: 52 Lives : 3 Hlth : 80 % Wtr : 20 Gld : 3 Oil Left : 2 Sonar : 1 Scr : 321000
+		std::string s;
+		s = "Lvl: " + (level > 10 ? "" : "_") + std::to_string(level) + "  Lives : "
+			+ std::to_string(lives) + "  Hlth : "
+			+ (health > 100 ? "" : "_") + health + "%  Wtr : "
+			+ (squirts > 10 ? "" : "_") + std::to_string(squirts) + "  Gld : "
+			+ (gold > 10 ? "" : "_") + std::to_string(gold) + "  Oil Left : "
+			+ (barrelsLeft > 10 ? "" : "_") + std::to_string(barrelsLeft) + "  Sonar : "
+			+ (sonar > 10 ? "" : "_") + std::to_string(sonar) + "  Scr : "
+			+ std::string(6 - std::to_string(score).length(), '0') + std::to_string(score);
+		GameWorld::setGameStatText(s); // in GameWorld.cpp
 	}
 
 private:
+	int gameLevel;
+	int lives;
+	int health;
+	int squirtGun;
+	int gold;
+	int barrels;
+	int sonarCharge;
+	int score;
 };
 
 #endif // STUDENTWORLD_H_
