4fdd187489d11b327a5eb39bded655890feed82d
lmpec <107778023+lmpec@users.noreply.github.com>
2025-05-28 13:53:47 -0700

    Update Actor.cpp

--- IceMan-skeleton-windows_v2/IceMan-skeleton-windows/IceMan-skeleton-windows/IceMan/IceMan/Actor.cpp
+++ IceMan-skeleton-windows_v2/IceMan-skeleton-windows/IceMan-skeleton-windows/IceMan/IceMan/Actor.cpp
@@ -1,8 +1,43 @@
 #include "Actor.h"
 #include "StudentWorld.h"
 
-int main () {
-    cout << "Hello, World!" << endl;
-    cout << "This is a test." << endl;
-}
 // Students:  Add code to this file (if you wish), Actor.h, StudentWorld.h, and StudentWorld.cpp
+//******************************** Actor Methods *******************************
+bool Actor::is_Alive(){
+    return m_isAlive;
+}
+
+
+
+//******************************** IceMan Methods *******************************
+void Iceman::doSomething(){
+   // if (this->is_Alive()){
+        
+        // if overlapping ice
+        
+        //check if key was pressed
+        
+            //different actions for different keys}
+    Direction d = getDirection();
+    setDirection(d);
+    int x = getX();
+    int y = getY();
+    moveTo(x, y);
+    return;
+}
+
+//******************************** Ice Methods *******************************
+
+
+//******************************** B.O.O. Methods *******************************
+
+
+//******************************** G Boulder Methods *******************************
+
+//******************************** Gold Nuggets Methods *******************************
+
+//******************************** Water Refills Methods *******************************
+
+//******************************** Hardcore Protestor Methods *******************************
+
+//******************************** Regular Protestor Methods *******************************
