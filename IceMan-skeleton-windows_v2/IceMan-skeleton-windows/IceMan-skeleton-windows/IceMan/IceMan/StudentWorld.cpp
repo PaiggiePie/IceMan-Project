c0b8241712890337a05fc740f2cf59de2e84b637
lmpec <107778023+lmpec@users.noreply.github.com>
2025-05-28 13:52:20 -0700

    Update StudentWorld.cpp
    semi complete init function

--- IceMan-skeleton-windows_v2/IceMan-skeleton-windows/IceMan-skeleton-windows/IceMan/IceMan/StudentWorld.cpp
+++ IceMan-skeleton-windows_v2/IceMan-skeleton-windows/IceMan-skeleton-windows/IceMan/IceMan/StudentWorld.cpp
@@ -1,10 +1,66 @@
 #include "StudentWorld.h"
+#include "Actor.h"
 #include <string>
+#include <vector>
 using namespace std;
 
 GameWorld* createStudentWorld(string assetDir)
 {
-	return new StudentWorld(assetDir);
+    return new StudentWorld(assetDir);
+}
+int StudentWorld::init(){
+    int i = getLives();
+    cout << "Lives:" << i << endl;
+    
+    Iceman* I = new Iceman;
+    I->setVisible(true);
+    
+    return GWSTATUS_CONTINUE_GAME;
+    
 }
 
-// Students:  Add code to this file (if you wish), StudentWorld.h, Actor.h and Actor.cpp
+int StudentWorld::move(){
+    
+    return GWSTATUS_CONTINUE_GAME;
+}
+
+
+/*
+int StudentWorld::move()
+{
+// Update the Game Status Line
+updateDisplayText(); // update the score/lives/level text at screen top
+// The term “Actors” refers to all Protesters, the player, Goodies,
+// Boulders, Barrels of oil, Holes, Squirts, the Exit, etc.
+// Give each Actor a chance to do something
+for each of the actors in the game world
+{
+if (actor[i] is still active/alive)
+{
+// ask each actor to do something (e.g. move)
+tellThisActorToDoSomething(actor[i]);
+if (theplayerDiedDuringThisTick() == true)
+return GWSTATUS_PLAYER_DIED;
+if (theplayerCompletedTheCurrentLevel() == true)
+{
+return GWSTATUS_FINISHED_LEVEL;
+}
+}
+}
+// Remove newly-dead actors after each tick
+removeDeadGameObjects(); // delete dead game objects
+// return the proper result
+if (theplayerDiedDuringThisTick() == true)
+return GWSTATUS_PLAYER_DIED;
+// If the player has collected all of the Barrels on the level, then
+// return the result that the player finished the level
+if (theplayerCompletedTheCurrentLevel() == true)
+{
+playFinishedLevelSound();
+return GWSTATUS_FINISHED_LEVEL;
+}
+// the player hasn’t completed the current level and hasn’t died
+// let them continue playing the current level
+return GWSTATUS_CONTINUE_GAME;
+}
+*/
