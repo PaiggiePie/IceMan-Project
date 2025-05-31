bb0a56b18036555cd0074c3ae433051a5d806665
PaiggiePie <paiggiepie@gmail.com>
2025-05-30 18:31:31 -0700

    Update StudentWorld.cpp

--- IceMan-skeleton-windows_v2/IceMan-skeleton-windows/IceMan-skeleton-windows/IceMan/IceMan/StudentWorld.cpp
+++ IceMan-skeleton-windows_v2/IceMan-skeleton-windows/IceMan-skeleton-windows/IceMan/IceMan/StudentWorld.cpp
@@ -4,55 +4,154 @@
 #include <vector>
 using namespace std;
 
+int ticks = 0; // global variable to keep track of ticks
 GameWorld* createStudentWorld(string assetDir)
 {
-	return new StudentWorld(assetDir);
+	
+    return new StudentWorld(assetDir);
+}
+
+StudentWorld::StudentWorld(std::string assetDir)
+    : GameWorld(assetDir) { //contructor
 
 
 }
 
+//functions to implement
 
-int StudentWorld::move()
-{
-	// Update the Game Status Line 
-	setDisplayText();
-	// update the score/lives/level text at screen top 
-	vector <Actor> Actors;
-	Actors.push_back(Iceman player);
-	// The term ìActorsî refers to all Protesters, the player, Goodies, 
-	// Boulders, Barrels of oil, Holes, Squirts, the Exit, etc. 
-	// Give each Actor a chance to do something 
-	for (int i = 0; i < Actors.size(); i++)// each of the actors in the game world
-	{
-		if (actor[i].is_alive() == true)  // is still active / alive
-		{
-			// ask each actor to do something (e.g. move) 
-			tellThisActorToDoSomething(actor[i]);
-			if (theplayerDiedDuringThisTick() == true)
-				return GWSTATUS_PLAYER_DIED;
-			if (theplayerCompletedTheCurrentLevel() == true)
-			{
-				return GWSTATUS_FINISHED_LEVEL;
-			}
+int StudentWorld::init() {
+	StudentWorld* sp = nullptr;
+    // make iceman
+    Iceman* iceMan = new Iceman(this);
+    actors.push_back(iceMan);
+
+    //ice making code
+    for (int i = 0; i < 65; i++) {
+        for (int j = 0; j < 60; j++) {
+            if (i > 29 && i < 34 && j > 3) {
+                continue;
+            }
+            else {
+                Ice* iceObj = new Ice(this);
+                iceObj->moveTo(i, j);
+                iceField.push_back(iceObj);
+            }
+
+        }
+    }
+
+	for (auto& iceObj : iceField) {
+        iceObj->setVisible(true);
+	}
+
+	// make static objects
+	/*BarrelsOfOil* barrel = new BarrelsOfOil(this);
+	GoldNuggets* nugget = new GoldNuggets(this);
+	Boulders* boulder = new Boulders(this);
+	actors.push_back(barrel);
+	actors.push_back(nuggets);
+	actors.push_back(boulders);*/
+
+    return GWSTATUS_CONTINUE_GAME;
+}
+
+
+int StudentWorld::move() {
+	//updateDisplayText();
+	//ticks++; // increment ticks for each move
+	// Give each Actor a chance to do something
+
+	for (auto& actor : actors) {
+		if (actor->is_Alive()) {
+			actor->doSomething();
+			//actor->animate(); // animate the actor
+		}
+		else {
+			delete actor; // delete dead actors
 		}
 	}
-	// Remove newly-dead actors after each tick 
-	removeDeadGameObjects(); // delete dead game objects 
-	// return the proper result 
-	if (theplayerDiedDuringThisTick() == true)
-		return GWSTATUS_PLAYER_DIED;
-	// If the player has collected all of the Barrels on the level, then 
-	// return the result that the player finished the level 
-	if (theplayerCompletedTheCurrentLevel() == true)
-	{
-		playFinishedLevelSound();
-		return GWSTATUS_FINISHED_LEVEL;
+	
+ //   int T = std::max(25, (200 - ((int) getLevel()))); // # of tick to wait for new protester
+	//int P = std::min(15.0, (2 + ((int) getLevel()) * 1.5)); // # of Protesters that should be on field
+	//int probabilityOfHardcore = std::min(90, ((int) getLevel() * 10 + 30));
+
+	//if (probabilityOfHardcore)
+
+
+	if (isGameOver() == true) {
+		return GWSTATUS_PLAYER_DIED; // if player died
 	}
-	// the player hasnít completed the current level and hasnít died 
-	// let them continue playing the current level 
-	return GWSTATUS_CONTINUE_GAME;
+	//if () {
+		//playFinishedLevelSound();
+		//return GWSTATUS_FINISHED_LEVEL;
+	//}
+    return GWSTATUS_CONTINUE_GAME;
 }
 
 void StudentWorld::cleanUp() {
+	// after a player has lost a life (with more lives remaining) or has completed a level,
+    // or lost all lives or game is over
+	// delete all actors and ice objects 
+
+	for (auto& actor : actors) {
+		delete actor; // delete all actors
+	}
+	actors.clear();
+	for (auto& iceObj : iceField) {
+		delete iceObj; // delete all ice objects
+	}
+	iceField.clear();
 
 }
+
+// useable functions
+//unsigned int getLives() const;
+//void decLives();
+//void incLives();
+//unsigned int getScore() const;
+//unsigned int getLevel() const;
+//void increaseScore(unsigned int howMuch);
+//void setGameStatText(string text);
+//bool getKey(int& value);
+//void playSound(int soundID);
+
+
+/*
+int StudentWorld::move()
+{
+// Update the Game Status Line
+updateDisplayText(); // update the score/lives/level text at screen top
+// The term ‚ÄúActors‚Äù refers to all Protesters, the player, Goodies,
+// Boulders, Barrels of oil, Holes, Squirts, the Exit, etc.
+// Give each Actor a chance to do something
+for each of the actors in the game world
+{
+if (actor[i] is still active/alive)
+{
+// ask each actor to do something (e.g. move)
+tellThisActorToDoSomething(actor[i]);
+if (theplayerDiedDuringThisTick() == true)
+return GWSTATUS_PLAYER_DIED;
+if (theplayerCompletedTheCurrentLevel() == true)
+{
+return GWSTATUS_FINISHED_LEVEL;
+}
+}
+}
+// Remove newly-dead actors after each tick
+removeDeadGameObjects(); // delete dead game objects
+// return the proper result
+if (theplayerDiedDuringThisTick() == true)
+return GWSTATUS_PLAYER_DIED;
+// If the player has collected all of the Barrels on the level, then
+// return the result that the player finished the level
+if (theplayerCompletedTheCurrentLevel() == true)
+{
+playFinishedLevelSound();
+return GWSTATUS_FINISHED_LEVEL;
+}
+// the player hasn‚Äôt completed the current level and hasn‚Äôt died
+// let them continue playing the current level
+return GWSTATUS_CONTINUE_GAME;
+}
+*/
