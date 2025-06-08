9a2e18723df599e98c172641db40dac5d91d0093
lmpec <107778023+lmpec@users.noreply.github.com>
2025-06-07 21:38:31 -0700

    Update StudentWorld.cpp

--- IceMan-skeleton-windows_v2/IceMan-skeleton-windows/IceMan-skeleton-windows/IceMan/IceMan/StudentWorld.cpp
+++ IceMan-skeleton-windows_v2/IceMan-skeleton-windows/IceMan-skeleton-windows/IceMan/IceMan/StudentWorld.cpp
@@ -4,94 +4,157 @@
 #include <vector>
 using namespace std;
 
+
+/*GameWorld* createStudentWorld(string assetDir){
+    return new StudentWorld(assetDir);
+}*/
+
+StudentWorld* sw;
+int ticks = 0; // global variable to keep track of ticks
+
 GameWorld* createStudentWorld(string assetDir)
 {
-    return new StudentWorld(assetDir);
+    sw = new StudentWorld(assetDir); // create a new StudentWorld object
+    return sw;
 }
 
 StudentWorld::StudentWorld(std::string assetDir)
 : GameWorld(assetDir){ //contructor
 
-    
+}
+
+StudentWorld::~StudentWorld(){
+    for (auto& actor : actors) {
+                delete actor; // delete all actors
+            }
+            actors.clear();
+            for (auto& iceObj : iceField) {
+                delete iceObj; // delete all ice objects
+            }
+            iceField.clear();
+            //cout << "StudentWorld dtor" << endl;
 }
 
 //functions to implement
 
+StudentWorld* Actor::getWorld(StudentWorld*& sp) const {
+    // This function returns the StudentWorld pointer associated with this Actor
+    return sp = sw;
+}
+
 int StudentWorld::init(){
     
-    //int i = getLives();
-    //cout << "Lives:" << i << endl;
-    
-    Iceman* iceMan = new Iceman(this);
-    actors.push_back(iceMan);
+    //StudentWorld* sp = nullptr;
 
-    //ice making code
+    Iceman* iceMan = new Iceman(sw);
+    actors.push_back(iceMan);
+        
     for (int i = 0; i < 65; i++){
         for (int j = 0; j < 60; j++){
             if(i > 29 && i < 34 && j > 3){
                 continue;
             }
             else{
-                Ice* iceObj = new Ice(this);
+                Ice* iceObj = new Ice(sw);
                 iceObj->moveTo(i, j);
                 iceField.push_back(iceObj);
             }
             
         }
     }
-        
-        
-    return GWSTATUS_CONTINUE_GAME;
     
+    for (auto& iceObj : iceField) {
+            iceObj->setVisible(true);
+        }
+    
+    return GWSTATUS_CONTINUE_GAME;
 }
 
+
 int StudentWorld::move(){
+    //ticks++; // increment ticks for each move
+    // Give each Actor a chance to do something
     
-    return GWSTATUS_CONTINUE_GAME;
+    for (auto& actor : actors) {
+        if (actor->isAlive()) {
+            actor->doSomething();
+            //actor->animate(); // animate the actor
+        }
+        else {
+            delete actor; // delete dead actors
+        }
+
+    }
+    //   int T = std::max(25, (200 - ((int) getLevel()))); // # of tick to wait for new protester
+       //int P = std::min(15.0, (2 + ((int) getLevel()) * 1.5)); // # of Protesters that should be on field
+       //int probabilityOfHardcore = std::min(90, ((int) getLevel() * 10 + 30));
+
+       //if (probabilityOfHardcore)
+     
+     if (isGameOver() == true) {
+     return GWSTATUS_PLAYER_DIED; // if player died
+     }
+     //if () {
+     //playFinishedLevelSound();
+     //return GWSTATUS_FINISHED_LEVEL;
+     //}
+     return GWSTATUS_CONTINUE_GAME;
 }
 
 void StudentWorld::cleanUp(){
-    
+    // after a player has lost a life (with more lives remaining) or has completed a level,
+    // or lost all lives or game is over
+    // delete all actors and ice objects
+
+    for (auto& actor : actors) {
+        delete actor; // delete all actors
+    }
+    actors.clear();
+    for (auto& iceObj : iceField) {
+        delete iceObj; // delete all ice objects
+    }
+    iceField.clear();
+
+}
+void StudentWorld::addActor(Actor* a){
+    actors.push_back(a);
 }
 
-/*
-int StudentWorld::move()
-{
-// Update the Game Status Line
-updateDisplayText(); // update the score/lives/level text at screen top
-// The term “Actors” refers to all Protesters, the player, Goodies,
-// Boulders, Barrels of oil, Holes, Squirts, the Exit, etc.
-// Give each Actor a chance to do something
-for each of the actors in the game world
-{
-if (actor[i] is still active/alive)
-{
-// ask each actor to do something (e.g. move)
-tellThisActorToDoSomething(actor[i]);
-if (theplayerDiedDuringThisTick() == true)
-return GWSTATUS_PLAYER_DIED;
-if (theplayerCompletedTheCurrentLevel() == true)
-{
-return GWSTATUS_FINISHED_LEVEL;
+
+void StudentWorld::clearIce(int x, int y){
+    for (auto& iceObj : iceField) {
+        if ((iceObj->getX() == x      ||
+            iceObj->getX() == x + 1   ||
+            iceObj->getX() == x + 2   ||
+            iceObj->getX() == x + 3)  &&
+            (iceObj->getY() == y      ||
+            iceObj->getY() == y + 1   ||
+            iceObj->getY() == y + 2   ||
+            iceObj->getY() == y + 3)){
+            iceObj->setVisible(false);
+            //playSound(SOUND_DIG);
+            iceObj->setDead();
+        }
+    }
 }
+
+bool StudentWorld::canActorMoveTo(Actor* a, int x, int y) const{
+    for (auto& actor : actors ){
+        if (actor->getX()== x && actor->getY() == y){
+            return false;
+        }
+    }
+    return true;
 }
+
+int StudentWorld::annoyAllNearbyActors(Actor* annoyer, int points, int radius){
+    //TODO
+    return 0;
 }
-// Remove newly-dead actors after each tick
-removeDeadGameObjects(); // delete dead game objects
-// return the proper result
-if (theplayerDiedDuringThisTick() == true)
-return GWSTATUS_PLAYER_DIED;
-// If the player has collected all of the Barrels on the level, then
-// return the result that the player finished the level
-if (theplayerCompletedTheCurrentLevel() == true)
-{
-playFinishedLevelSound();
-return GWSTATUS_FINISHED_LEVEL;
+
+void StudentWorld::revealAllNearbyObjects(int x, int y, int radius){
+    //TODO
+    
 }
-// the player hasn’t completed the current level and hasn’t died
-// let them continue playing the current level
-return GWSTATUS_CONTINUE_GAME;
-}
-*/
 
 
