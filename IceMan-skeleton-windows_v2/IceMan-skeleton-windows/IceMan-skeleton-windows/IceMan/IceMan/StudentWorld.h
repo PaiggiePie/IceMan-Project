abf068f6e52dc88ea828747392ee9433fdf159db
lmpec <107778023+lmpec@users.noreply.github.com>
2025-06-09 11:27:35 -0700

    Update StudentWorld.h

--- IceMan-skeleton-windows_v2/IceMan-skeleton-windows/IceMan-skeleton-windows/IceMan/IceMan/StudentWorld.h
+++ IceMan-skeleton-windows_v2/IceMan-skeleton-windows/IceMan-skeleton-windows/IceMan/IceMan/StudentWorld.h
@@ -23,87 +23,102 @@ public:
     
     ~StudentWorld();
     
-    
-    
+    //our defined functions
+    virtual Iceman* getIceman() const;
+    virtual void setWorld(StudentWorld*& sp);
+    void setIceman(Iceman* a);
+
+    // Add an agent to the world.
+    void addObj(ActivatingObject* a);
+
     // Add an actor to the world.
-        void addActor(Actor* a);
-        
-          // Clear a 4x4 region of Ice.
-        void clearIce(int x, int y);
-
-          // Can actor move to x,y?
-        bool canActorMoveTo(Actor* a, int x, int y) const;
-
-          // Annoy all other actors within radius of annoyer, returning the
-          // number of actors annoyed.
-        int annoyAllNearbyActors(Actor* annoyer, int points, int radius);
-
-          // Reveal all objects within radius of x,y.
-        void revealAllNearbyObjects(int x, int y, int radius);
-
+    void addActor(Actor* a);
     
-          // If the IceMan is within radius of a, return a pointer to the
-          // IceMan, otherwise null.
-        Actor* findNearbyIceMan(Actor* a, int radius) const;
+      // Clear a 4x4 region of Ice.
+    void clearIce(int x, int y);
 
-          // If at least one actor that can pick things up is within radius of a,
-          // return a pointer to one of them, otherwise null.
-        Actor* findNearbyPickerUpper(Actor* a, int radius) const;
+      // Can actor move to x,y?
+    bool canActorMoveTo(Actor* a, int x, int y) const;
 
-          // Annoy the IceMan.
-        void annoyIceMan();
+      // Annoy all other actors within radius of annoyer, returning the
+      // number of actors annoyed.
+    int annoyAllNearbyActors(Actor* annoyer, int points, int radius);
 
-          // Give IceMan some sonar charges.
-        void giveIceManSonar();
+      // Reveal all objects within radius of x,y.
+    void revealAllNearbyObjects(int x, int y, int radius);
 
-          // Give IceMan some water.
-        void giveIceManWater();
 
-          // Is the Actor a facing toward the IceMan?
-        bool facingTowardIceMan(Actor* a) const;
+      // If the IceMan is within radius of a, return a pointer to the
+      // IceMan, otherwise null.
+    Actor* findNearbyIceMan(Actor* a, int radius) const;
 
-          // If the Actor a has a clear line of sight to the IceMan, return
-          // the direction to the IceMan, otherwise GraphObject::none.
-        GraphObject::Direction lineOfSightToIceMan(Actor* a) const;
+      // If at least one actor that can pick things up is within radius of a,
+      // return a pointer to one of them, otherwise null.
+    //Agent* findNearbyPickerUpper(Agent* a, int radius) const;
+    Agent* findNearbyPickerUpper(Actor* a, int radius) const;
 
-          // Return whether the Actor a is within radius of IceMan.
-        bool isNearIceMan(Actor* a, int radius) const;
+      // Annoy the IceMan.
+    void annoyIceMan();
 
-          // Determine the direction of the first move a quitting protester
-          // makes to leave the oil field.
-        GraphObject::Direction determineFirstMoveToExit(int x, int y);
+      // Give IceMan some sonar charges.
+    void giveIceManSonar();
 
-          // Determine the direction of the first move a hardcore protester
-          // makes to approach the IceMan.
-        GraphObject::Direction determineFirstMoveToIceMan(int x, int y);
-    
+      // Give IceMan some water.
+    void giveIceManWater();
+
+      // Is the Actor a facing toward the IceMan?
+    bool facingTowardIceMan(Actor* a) const;
+
+      // If the Actor a has a clear line of sight to the IceMan, return
+      // the direction to the IceMan, otherwise GraphObject::none.
+    GraphObject::Direction lineOfSightToIceMan(Actor* a) const;
+
+      // Return whether the Actor a is within radius of IceMan.
+    bool isNearIceMan(Actor* a, int radius) const;
+
+      // Determine the direction of the first move a quitting protester
+      // makes to leave the oil field.
+    GraphObject::Direction determineFirstMoveToExit(int x, int y);
+
+      // Determine the direction of the first move a hardcore protester
+      // makes to approach the IceMan.
+    GraphObject::Direction determineFirstMoveToIceMan(int x, int y);
+
 
 
     
     //getter functions
-    virtual const int getCurrentGameLevel() { return gameLevel; }
-    virtual const int getNumLivesLeft() { return lives; }
-    virtual const int getCurrentHealth() { return health; }
-    virtual const int getSquirtsLeftInSquirtGun() { return squirtGun;}
-    virtual const int getPlayerGoldCount() { return gold; }
-    virtual const int getNumberOfBarrelsRemainingToBePickedUp() { return barrels; }
-    virtual const int getPlayerSonarChargeCount() { return sonarCharge; }
-    virtual const int getCurrentScore() { return score; }
+    virtual const int getCurrentHealth() { return iceman->getHitPoints(); };
+    virtual const int getSquirtsLeftInSquirtGun() { return iceman->getWater(); };
+    virtual const int getPlayerGoldCount() { return iceman->getGold(); };
+    virtual const int getBarrelsRemaining() {
+        int barrels = 0;
+        for (int i = 0; i < aobj.size(); i++) {
+            if (aobj[i]->getID() == IID_BARREL) {
+                barrels++;
+            }
+        }
+        cout << barrels  << endl;
+        return barrels; // if no barrels, return 0
+    };
+    
+    virtual const int getPlayerSonarChargeCount() { return iceman->getSonar(); };
+    
+    virtual const int getTicks() const;
 
     //setter functions
-    
     virtual void setDisplayText() {
-        int level = getCurrentGameLevel();
-        int lives = getNumLivesLeft();
+        int level = getLevel();
+        int lives = getLives();
         int health = getCurrentHealth();
         int squirts = getSquirtsLeftInSquirtGun();
         int gold = getPlayerGoldCount();
-        int barrelsLeft = getNumberOfBarrelsRemainingToBePickedUp();
+        int barrelsLeft = getBarrelsRemaining();
         int sonar = getPlayerSonarChargeCount();
-        int score = getCurrentScore();
+        int score = getScore();
+        
         // Next, create a string from your statistics, of the form:
         // Lvl: 52 Lives : 3 Hlth : 80 % Wtr : 20 Gld : 3 Oil Left : 2 Sonar : 1 Scr : 321000
-    
         std::string s;
         s = "Lvl: " + (string) (level > 10 ? "" : "_") + std::to_string(level) + "  Lives : "
             + std::to_string(lives) + "  Hlth : "
@@ -118,22 +133,12 @@ public:
 
 private:
 
-    
-    std::vector <Actor*> actors {}; //ice boulders, squirt
-    std::vector<Agent*> agents {}; // Icemean, Protesters
-    std::vector<ActivatingObject*> aobj; // barrels, gold, sonar, waterpool
-    std::vector <Ice*> iceField;
-    
-    int ticks = 0;
-    int currentLevelNumber;
-    int gameLevel;
-    int health;
-    int lives;
-    int squirtGun;
-    int gold;
-    int barrels;
-    int sonarCharge;
-    int score;
+    Iceman* iceman = nullptr;
+    std::vector<Agent*> agents; // Protesters & iceman
+    std::vector<ActivatingObject*> aobj; // for barrels, gold, sonar, waterpools
+    std::vector<Actor*> iceField; // ice, boulders, squirts
+    int ticks = 1; // global variable to keep track of ticks
+    int currentLevelNumber = 1;
     
 };
 
